ebd82b544f6cd70a2174483ea35abdd4d4aba63a
wjlee <wjlee.ling@gmail.com>
2024-11-19 13:55:22 +0900

    update: LLM calling per chunks and donwloading

--- pages/Parse.py
+++ pages/Parse.py
@@ -1,6 +1,7 @@
 from chains.example_selector import (
     CustomExampleSelector,
     example_prompt,
+    get_example_selector_prompt,
     get_example_selector_chain_with_structured_output,
 )
 from langchain_core.prompts.few_shot import FewShotPromptTemplate
@@ -44,33 +45,51 @@ def get_data(csv):
     return df
 
 
-if sst.csv or True:
-    # sst.df = get_data(sst.csv)
+if sst.csv:
+    sst.df = get_data(sst.csv)
     with st.spinner("Pinecone에 연결 중"):
         init()
 
-    # with st.expander("DataFrame 분석", expanded=True):
-    #     st.write("columns")
-    #     st.info(sst.df.columns)
-    #     st.write("DataFrame shape")
-    #     st.info(f"{sst.df.shape[0]}행")
+    with st.expander("DataFrame 분석", expanded=True):
+        st.write("columns")
+        st.info(sst.df.columns)
+        st.write("DataFrame shape")
+        st.info(f"{sst.df.shape[0]}행")
 
-    example_selector_prompt = FewShotPromptTemplate(
-        example_selector=sst.example_selector,
-        example_prompt=example_prompt,
-        suffix="input:\n{input}\noutput:\n",
-        prefix="Your task is to, given a new query as input, classify its intent and label.\nYou will be given previous parsed results to which you can refer to parse the new query. If you are uncertain with the intent, leave it an empty string.",
-        input_variables=["input"],
-    )
-
+    example_selector_prompt = get_example_selector_prompt(sst.example_selector)
     example_selector_chain = get_example_selector_chain_with_structured_output(
         example_selector_prompt, sst.llm
     )
 
-    inputs = [
-        {"input": "할부는 몇 개월까지 가능한가요?"},
-        {"input": "전액 현대캐피탈로 할부하려면 캐피탈에 미리 처리해 놓아야 하나요?"},
-    ]
+    with st.form("설정"):
+        text_col = st.selectbox(
+            label="임베딩 및 분류할 column",
+            options=sst.df.columns.tolist(),
+            index=sst.df.columns.tolist().index("Q"),
+        )
+        submit = st.form_submit_button("라벨링 시작")
 
-    output = example_selector_chain.batch(inputs)
-    st.info(output)
+    if submit:
+        chunk_size = 10
+        chunked_dfs = [
+            sst.df.iloc[i : i + chunk_size] for i in range(0, len(sst.df), chunk_size)
+        ]
+        dfs = []
+        with st.spinner("LLM 작업 중"):
+            for chunk_df in chunked_dfs:
+                outputs = example_selector_chain.batch(chunk_df[text_col].tolist())
+                chunk_df["hyundai_label"] = [o["hyundai_label"] for o in outputs]
+                chunk_df["intent"] = [o["intent"] for o in outputs]
+                dfs.append(chunk_df)
+
+                # st.dataframe(chunk_df)
+                # for output in outputs:
+                #     st.info(output)
+
+        total_df = pd.concat(dfs)
+        if total_df.shape[0] > 0 and st.download_button(
+            "작업된 csv 다운로드",
+            total_df.to_csv().encode("utf-8"),
+            file_name=f"{sst.csv.name}_라벨링.csv",
+        ):
+            st.info("다운로드 완료")
